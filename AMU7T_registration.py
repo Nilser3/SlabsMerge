ed2ff61fccf02bb5a325d41ea9ebae1985d3f74d
Nilser Laines Medina <77469192+Nilser3@users.noreply.github.com>
2024-02-20 09:59:43 -0500

    Update AMU7T_registration.py

--- AMU7T_registration.py
+++ AMU7T_registration.py
@@ -17,8 +17,8 @@ def main(args):
     comand_3 = f"sct_register_to_template -i {args.path_output}/image_crop.nii.gz -s {args.path_output}/image_wm_crop.nii.gz -l {args.landmarks} -c t1 -v 1 -s-template-id 4  -t {args.path_template_AMU7T}  -param step=1,type=imseg,algo=centermassrot,rot_method=pcahog:step=2,type=seg,algo=bsplinesyn,slicewise=0,metric=MeanSquares,samplStrategy=None,samplPercent=0.2,iter=2,smooth=1,rot_method=pcahog:step=3,type=seg,algo=syn,metric=MeanSquares,shrink=2,dof=Tz_Rz_Sz,slicewise=1,iter=20 -ref subject -ofolder {args.path_output}"
     os.system(comand_3)
     # Registration of AMU7T atlas to subject space
-    #comand_4 = f"sct_apply_transfo -i {args.path_template_AMU7T}/atlas/AMU7T_50_labels.nii.gz -d {args.path_output}/image_crop.nii.gz  -w {args.path_output}/warp_template2anat.nii.gz -o {args.path_output}/labels_AMU7T2anat.nii.gz -x nn"
-    #os.system(comand_4)
+    comand_4 = f"sct_apply_transfo -i {args.path_template_AMU7T}/atlas/AMU7T_50_labels.nii.gz -d {args.path_output}/image_crop.nii.gz  -w {args.path_output}/warp_template2anat.nii.gz -o {args.path_output}/labels_AMU7T2anat.nii.gz -x nn"
+    os.system(comand_4)
     comand_5 = f"sct_warp_template -d {args.path_output}/image_crop.nii.gz -w {args.path_output}/warp_template2anat.nii.gz  -t {args.path_template_AMU7T} -ofolder {args.path_output}"
     os.system(comand_5)
 
